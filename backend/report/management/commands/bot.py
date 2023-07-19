2e3763243e37f8a50ba657e8e7206b89fe5f61c0
Илья Афанасьев <iafanasevmail@gmail.com>
2023-07-19 23:59:34 +0300

    added handlers

--- backend/report/management/commands/bot.py
+++ backend/report/management/commands/bot.py
@@ -1,15 +1,36 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import (
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    Updater,
+)
 from telegram import ReplyKeyboardMarkup
 
 from report.models import Employee
 
 
+ORDER, ITEM_ORDER, EXECUTION_TIME, IMAGE = range(4)
+
+
 def work_report(update, context):
-    chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Отчет о работе')
+    update.message.reply_text('Введите номер счета что бы продолжить')
+    return ORDER
+
+
+def order_handler(update, context):
+    context.user_data[ORDER] = update.message.text
+    update.message.reply_text('Введите порядковый номер позиции из счета')
+    return ITEM_ORDER
+
+
+def cancel_handler(update, context):
+    """Cancels and ends the conversation."""
+    update.message.reply_text('Bye! I hope we can talk again some day.')
+    return ConversationHandler.END
 
 
 def after_work_report(update, context):
@@ -35,12 +56,29 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         updater = Updater(token=settings.TELEGRAM_TOKEN)
-        updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-        updater.dispatcher.add_handler(
-            CommandHandler('work_report', work_report),
+
+        report_handler = ConversationHandler(
+            entry_points=[
+                CommandHandler('work_report', work_report),
+            ],
+            states={
+                ORDER: [
+                    MessageHandler(Filters.all, order_handler, pass_user_data=True),
+                ],
+                # ITEM_ORDER: [
+                #     MessageHandler(Filters.all, pass_user_data=True),
+                # ],
+            },
+            fallbacks=[
+                CommandHandler('cancel', cancel_handler),
+            ],
         )
+
+        updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+        updater.dispatcher.add_handler(report_handler)
         updater.dispatcher.add_handler(
             CommandHandler('after_work_report', after_work_report)
         )
+
         updater.start_polling()
         updater.idle()
