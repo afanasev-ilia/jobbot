93d9278212bc960032cec48f34548833f67b00eb
Илья Афанасьев <iafanasevmail@gmail.com>
2023-07-24 22:07:24 +0300

    added type hints

--- backend/report/management/commands/bot.py
+++ backend/report/management/commands/bot.py
@@ -1,14 +1,16 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
+from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import (
     CommandHandler,
     ConversationHandler,
+    ContextTypes,
     Filters,
     MessageHandler,
     Updater,
 )
-from telegram import ReplyKeyboardMarkup
+
 
 from report.models import Employee
 
@@ -16,44 +18,50 @@ from report.models import Employee
 ORDER, ITEM_ORDER, EXECUTION_TIME, IMAGE = range(4)
 
 
-def work_report(update, context):
+def work_report(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     update.message.reply_text('Введите номер счета что бы продолжить')
     return ORDER
 
 
-def order_handler(update, context):
+def order_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data[ORDER] = update.message.text
     update.message.reply_text('Введите порядковый номер позиции из счета')
     return ITEM_ORDER
 
 
-def item_handler(update, context):
+def item_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data[ITEM_ORDER] = update.message.text
     update.message.reply_text('Укажите время выполнения в минутах')
     return EXECUTION_TIME
 
 
-def time_handler(update, context):
+def time_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data[EXECUTION_TIME] = update.message.text
     update.message.reply_text('Приложите фотографию')
     return IMAGE
 
 
-def image_handler(update, context):
+def image_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     print(context.user_data)
 
 
-def cancel_handler(update, context):
-    update.message.reply_text('Bye! I hope we can talk again some day.')
+def cancel_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    update.message.reply_text('Отчет не отправлен')
     return ConversationHandler.END
 
 
-def after_work_report(update, context):
+def after_work_report(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE,
+) -> int:
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Отчет в конце дня')
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Отчет об уборке рабочего места',
+    )
 
 
-def wake_up(update, context):
+def wake_up(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     chat = update.effective_chat
     Employee.objects.get_or_create(external_id=chat.id)
     button = ReplyKeyboardMarkup(
