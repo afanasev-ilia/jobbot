7aca0884ae6c596d60f4fd92cb0cafc67d79bc0c
Илья Афанасьев <iafanasevmail@gmail.com>
2023-08-20 21:07:42 +0300

    added clean report views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from api.apps import ApiConfig
-from api.views import report_list
+from api.views import clean_report_list, work_report_list
 
 
 app_name = ApiConfig.name
 
 urlpatterns = [
-   path('v1/reports/', report_list),
+   path('v1/clean_reports/', clean_report_list),
+   path('v1/work_reports/', work_report_list),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,23 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.serializers import WorkReportSerializer
+from api.serializers import CleanReportSerializer, WorkReportSerializer
 
 
 @api_view(['POST'])
-def report_list(request):
+def work_report_list(request):
     if request.method == 'POST':
         serializer = WorkReportSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def clean_report_list(request):
+    if request.method == 'POST':
+        serializer = CleanReportSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
