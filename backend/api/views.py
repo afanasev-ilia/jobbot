7a0a21a13aa8c53c2d07eaf5ef805c2c55464453
Илья Афанасьев <iafanasevmail@gmail.com>
2023-10-11 23:42:49 +0300

    added api.views type hints

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,19 +7,19 @@ from api.serializers import CleanReportSerializer, WorkReportSerializer
 
 
 @api_view(['POST'])
-def work_report_list(request: Request) -> Response:
+def work_report_list(request: Request) -> Response: # type: ignore
     if request.method == 'POST':
-        serializer = WorkReportSerializer(data=request.data)
+        serializer = WorkReportSerializer(data=request.data) # type: ignore
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)      
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
-def clean_report_list(request: Request) -> Response:
+def clean_report_list(request: Request) -> Response: # type: ignore
     if request.method == 'POST':
-        serializer = CleanReportSerializer(data=request.data)
+        serializer = CleanReportSerializer(data=request.data)  # type: ignore
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
